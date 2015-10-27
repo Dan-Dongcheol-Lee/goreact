860c4e3ca53ade7da736b9e2ffb532f3b609a72f
Dan Dongcheol Lee <leedc123@gmail.com>
2015-10-27 22:37:49 +1100

    Apply browerify and babelify for react components

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('clean', function(cb) {
 });
 
 gulp.task('build', function () {
-  var testFiles = glob.sync('./web/comp/**/*.js');
+  var testFiles = glob.sync('./src/**/*.js');
   return browserify({
     entries: testFiles,
     extensions: ['.js'],
@@ -21,28 +21,25 @@ gulp.task('build', function () {
   })
   .transform(babelify)
   .bundle()
-  .pipe(source('app.js'))
+  .pipe(source('bundle.js'))
   .pipe(plumber())
   .pipe(gulp.dest('./web'));
 });
 
 gulp.task('copy', ['clean'], function () {
-  gulp.src('./src/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./web'));
   return gulp.src([
     './node_modules/*/dist/**/*', './bower_components/*/dist/**/*', '!./**/*.min.*', '!./**/npm.*'])
     .pipe(gulp.dest('./web/lib'));
 });
 
-gulp.task('index', ['copy'], function () {
+gulp.task('index', ['copy', 'build'], function () {
   return gulp.src('./src/index.html')
     .pipe(inject(gulp.src([
       './web/lib/react/dist/react.js',
       './web/lib/react-dom/dist/react-dom.js'
     ], {read: false}), {name: 'lib'}))
     .pipe(inject(gulp.src(['./web/lib/**/*']), {name: 'lib'}))
-    .pipe(inject(gulp.src(['./web/comp/**/*', './web/app.js']), {name: 'app'}))
+    .pipe(inject(gulp.src('./web/bundle.js'), {name: 'app'}))
     .pipe(gulp.dest('./web'));
 });
 
